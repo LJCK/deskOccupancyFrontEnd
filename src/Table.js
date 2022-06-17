c758c5af4a55c4afce959749b43c5b4c77863ad5
LJCK <50812564+LJCK@users.noreply.github.com>
2022-06-17 16:59:48 +0800

    first time

---
+++ src/Table.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import axios from 'axios'
+import Button from '@mui/material/Button';
+
+const Table=()=>{
+  const [tableStatus, setTableStatus] = useState()
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+
+    axios.get("http://localhost:3000/table/1").then((res)=>{setTableStatus(res.data)})
+  }
+
+  return (
+    <>
+      <Button variant="contained" onClick={handleSubmit}>table 1</Button>
+      <h3>{tableStatus}</h3>
+    </>
+  )
+}
+
+export default Table
