9a9fbe2e147863ea1350904b4c5aceebec65f7a4
LJCK <50812564+LJCK@users.noreply.github.com>
2022-07-06 10:36:35 +0800

    Update Table.js

--- src/Table.js
+++ src/Table.js
@@ -3,12 +3,12 @@ import axios from 'axios'
 import Button from '@mui/material/Button';
 
 const Table=()=>{
-  const [tableStatus, setTableStatus] = useState()
+  const [tableStatus, setTableStatus] = useState([])
 
   const handleSubmit = (e) =>{
     e.preventDefault()
 
-    axios.get("http://localhost:3000/table/1").then((res)=>{setTableStatus(res.data)})
+    axios.get("http://localhost:3001/table/67",tableStatus).then((res)=>{setTableStatus(res.data)})
   }
 
   return (
