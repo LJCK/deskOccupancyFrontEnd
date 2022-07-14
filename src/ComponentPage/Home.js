15ffeca8d5b6974a076a019ad44ef104b52b6fe4
LJCK <50812564+LJCK@users.noreply.github.com>
2022-07-14 17:01:32 +0800

    update

--- src/ComponentPage/Home.js
+++ src/ComponentPage/Home.js
@@ -1,10 +1,15 @@
-import React from 'react'
-
+import React,{useState, useRef, useEffect} from 'react'
+import data from '../images/data'
+// import Test from '../images/test'
+// import {ReactComponent as Tables} from '../images/table.svg'
+import Tables from '../images/Tables'
 
 export const Home = () => {
+  const [tableStatus,setTableStatus] = useState(data)
+  // const tableStatus = useRef(data)
   return (
     <div>
-      <tables/>
+      <Tables table1_col = 'red' tableObj = {tableStatus}/>
     </div>
   )
 }
---
+++ src/images/data.js
@@ -0,0 +1,50 @@
+export default[
+  {
+    _id:"nv_ta_l8_1",
+    location:"Novena Tower A",
+    level:{"$numberInt":"8"},
+    occupancyStatus:"occupied",
+    expiryTime:"15:03:51",
+    __v:{"$numberInt":"0"}
+  },
+  {
+    _id:"nv_ta_l8_2",
+    location:"Novena Tower A",
+    level:{"$numberInt":"8"},
+    occupancyStatus:"occupied",
+    expiryTime:"15:03:51",
+    __v:{"$numberInt":"0"}
+  },
+  {
+    _id:"nv_ta_l8_3",
+    location:"Novena Tower A",
+    level:{"$numberInt":"8"},
+    occupancyStatus:"unoccupied",
+    expiryTime:"15:03:51",
+    __v:{"$numberInt":"0"}
+  },
+  {
+    _id:"nv_ta_l8_4",
+    location:"Novena Tower A",
+    level:{"$numberInt":"8"},
+    occupancyStatus:"occupied",
+    expiryTime:"15:03:51",
+    __v:{"$numberInt":"0"}
+  },
+  {
+    _id:"nv_ta_l8_5",
+    location:"Novena Tower A",
+    level:{"$numberInt":"8"},
+    occupancyStatus:"unoccupied",
+    expiryTime:"15:03:51",
+    __v:{"$numberInt":"0"}
+  },
+  {
+    _id:"nv_ta_l8_6",
+    location:"Novena Tower A",
+    level:{"$numberInt":"8"},
+    occupancyStatus:"occupied",
+    expiryTime:"15:03:51",
+    __v:{"$numberInt":"0"}
+  }
+]
