e4e5756fbb5185b16b2d872dda74f7b96d9dc543
qinxxxm <soldiero3of@gmail.com>
2022-09-06 18:00:19 +0800

    Create AuthContext.js

---
+++ src/context/AuthContext.js
@@ -0,0 +1,18 @@
+import { createContext, useReducer } from 'react'
+
+export const AuthContext = createContext()
+
+export const authReducer = (state, action) => {
+    switch(action.type) {
+        case 'LOGIN':
+            return { user: action.payload }
+        case 'LOGOUT':
+            return { user: null }
+    }
+}
+
+export const AuthContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(authReducer, {
+        user: null
+    })
+}
