6500b80e110c31892969a2fa103289052dd3c80c
Liang Zhiheng <liangzhiheng62@gmail.com>
2022-09-22 11:08:41 +0800

    Update Home.js

--- src/ComponentPage/Home.js
+++ src/ComponentPage/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 
 export const Home = () => {
-
+  console.log("this is home, api url is ", process.env.REACT_APP_API_URL)
   return (
     <div style={{paddingLeft:"1rem",paddingRight:"1rem"}}>
       <h1>Home Page</h1>
