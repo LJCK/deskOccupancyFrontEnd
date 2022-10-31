ea183b4b2be5569432c3f788f81e6452cc2424af
Liang Zhiheng <liangzhiheng62@gmail.com>
2022-10-31 08:33:29 +0800

    Revert "update"
    This reverts commit f3b63a11189451803500940d627d3018b041dc08.

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -9,7 +9,7 @@ export const useLogin = () => {
     const login = async (email, password) => {
         setIsLoading(true)
         setError(null)
-        console.log(process.env.REACT_APP_API_URL)
+
         const response = await fetch(`${process.env.REACT_APP_API_URL}/user/login`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
