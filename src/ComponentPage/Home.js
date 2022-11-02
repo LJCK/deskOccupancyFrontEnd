5c898f399e994e46ff45df5d2559d09bb3ad06d0
Liang Zhiheng <liangzhiheng62@gmail.com>
2022-11-02 09:44:14 +0800

    Update Home.js

--- src/ComponentPage/Home.js
+++ src/ComponentPage/Home.js
@@ -1,8 +1,17 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 
 export const Home = () => {
+
+  const {text, setText} = useState([])
+
+  useEffect(()=>{
+    axios.get(`${process.env.REACT_APP_API_URL}/getFile?filename=text.txt`).then((res)=>{
+      console.log(res.data)
+    })
+  },[])
 
   return (
     <div style={{paddingLeft:"1rem",paddingRight:"1rem"}}>
